e8c09000190938a278463a8aa49fd7a328d82213
theodore <thinhtran3803@gmail.com>
2022-03-20 14:16:22 +0700

    Add new files and folder

---
+++ Project 1/main.js
@@ -0,0 +1,10 @@
+// function writeLog() {
+// 	var myString = "";
+// 	for (var param of arguments){
+//         myString += `${param} - `;
+// 	}
+//     console.log(myString)
+// }
+// writeLog('thinh1','thinh2');
+var firstName='Tran', lastName='Thinh';
+console.log(firstName, lastName);
